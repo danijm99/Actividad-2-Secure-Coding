e8e91cf4016195289defb79a1d9eaaeb7c881e32
Daniel Jiménez <117750650+danijm99@users.noreply.github.com>
2026-06-22 01:51:41 +0200

    Add Content-Security-Policy header to middleware

--- src/go/handlers/middleware.go
+++ src/go/handlers/middleware.go
@@ -22,6 +22,8 @@ func SecurityHeadersMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Frame-Options", "DENY")
 
+		w.Header().Set("Content-Security-Policy", "frame-ancestors 'none'")
+
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 
 		w.Header().Set("Permissions-Policy", "camera=(), microphone=(), geolocation=()")
